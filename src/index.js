994b341feac45a781384a01d0e0733768190ad2b
Nick Schnelle <nschnelle@odecee.com.au>
2017-11-04 22:55:57 +1100

    chore(app): initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Test
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
