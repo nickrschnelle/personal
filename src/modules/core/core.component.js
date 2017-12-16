4c56bfff020cc5214576d6026fc73f33d5390138
Nick Schnelle <nrsch@student.monash.edu>
2017-12-17 00:40:58 +1100

    feat(header): add toggling hamburger icon, github link

--- src/modules/core/core.component.js
+++ src/modules/core/core.component.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route } from 'react-router-dom';
+import styles from './core.styl';
 
 import Header from '../header/header.component';
+import Nav from '../header/nav/nav.component';
 import Main from '../main/main.component';
 import About from '../about/about.component';
 
 export default class Core extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isResponsiveNavOpen: false
+    };
+  }
+
+  toggleHamburger() {
+    this.setState({isResponsiveNavOpen: !this.state.isResponsiveNavOpen});
+  }
+
   render() {
     return (
       <div style={{height: '100%'}}>
-        <Header/>  
+        <Header toggleHamburger={this.toggleHamburger.bind(this)}/>
+        <div className={this.state.isResponsiveNavOpen ? styles.showNav : styles.hideNav}>
+          <Nav />
+        </div>
         <Route exact path="/" render={() => <Main /> } />
         <Route path="/about" render={() => <About /> } />
         <Route path="/snippets" render={() => <Snippets /> } />
--- src/modules/header/header.component.js
+++ src/modules/header/header.component.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 import styles from './header.styl';
 
 import Linkedin from '../../common/svgs/linkedin.svg';
+import Github from '../../common/svgs/github.svg';
+import Hamburger from '../../common/svgs/hamburger.svg';
 
 import Nav from './nav/nav.component';
 
@@ -11,15 +14,20 @@ export default class Header extends React.Component {
     return (
       <header>
         <div className={styles.homeLink}>
-          <p><a href="">n<span>ick </span>s<span>chnelle</span></a></p>
+          <p><Link to="/">n<span>ick </span>s<span>chnelle</span></Link></p>
         </div>
         <Nav />
         <div className={styles.social}>
+          <a href="https://github.com/nickrschnelle/" rel="noopener noreferrer" target="_blank">
+            <Github className={styles.icon}/>
+          </a>
           <a href="https://www.linkedin.com/in/nick-schnelle-959765a9" rel="noopener noreferrer" target="_blank">
-            <Linkedin className={`${styles.icon} ${styles.iconLinkedin}`}/>
+            <Linkedin className={styles.icon}/>
           </a>
         </div>
-        <div className={styles.hamburger}>hamburger</div>
+        <div className={styles.hamburger}>
+          <button onClick={this.props.toggleHamburger} className={styles.svgButton}><Hamburger className={styles.icon}/></button>
+        </div>
       </header>
     );
   }
